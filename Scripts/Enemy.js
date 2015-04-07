aedfc62d40930822f86bd257062470c01d442fab
DanielHeath1234 <minidan98@iinet.net.au>
2015-04-07 16:55:56 +1000

    Added Borders and Character Flipping

--- Scripts/Enemy.js
+++ Scripts/Enemy.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById("gameCanvas");
+var flipEnemy = false;
 
 var Enemy = function() {
 	this.image = document.createElement("img");
@@ -6,8 +7,8 @@ var Enemy = function() {
 	this.xPos = canvas.width/2;
 	this.yPos = canvas.height/2;
 	
-	this.width = 159;
-	this.height = 163;
+	this.width = 119;
+	this.height = 103;
 	
 	this.velocityX = 0;
 	this.velocityY = 0;
@@ -20,16 +21,48 @@ var Enemy = function() {
 };
 
 Enemy.prototype.update = function(deltaTime){
-	if(this.xPos >= canvas.width + (this.width / 2)){
-		this.xPos = 0 - (this.width / 2)
+	if(this.xPos > canvas.width - (this.width / 2)){
+		this.xPos = canvas.width - (this.width / 2);
 	}
-	this.xPos += deltaTime * 300;
+	if(this.xPos < 0 + (this.width / 2)){
+		this.xPos = 0 + (this.width / 2);
+	}
+	if(this.yPos > canvas.height - (this.height / 2)){
+		this.yPos = canvas.height - (this.height / 2);
+	}
+	if(this.yPos < 0 + (this.width / 2) - 7){
+		this.yPos = 0 + (this.width / 2) - 7;
+	}
+	
+	if(keyboard.isKeyDown(keyboard.KEY_UP) == true){
+		this.yPos -= 5;
+	}
+	if(keyboard.isKeyDown(keyboard.KEY_DOWN) == true){
+		this.yPos += 5;
+	}
+	if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true){
+		this.xPos -= 5;
+		flipEnemy = true;
+	}
+	if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true){
+		this.xPos += 5;
+		flipEnemy = false;
+	}
 }
 
 Enemy.prototype.draw = function(){
-	context.save();
-		context.translate(this.xPos, this.yPos);
-		context.rotate(this.rotation);
-		context.drawImage(this.image, -this.width/2, -this.height/2);
-	context.restore();
+	if(flipEnemy == true){
+		context.save();
+			context.translate(this.width, 0);
+			context.translate(this.xPos, this.yPos);
+			context.scale(-1, 1);
+			context.drawImage(this.image, +this.width/2, -this.height/2);
+		context.restore();
+	}else{
+		context.save();
+			context.translate(this.xPos, this.yPos);
+			context.rotate(this.rotation);
+			context.drawImage(this.image, -this.width/2, -this.height/2);
+		context.restore();
+	}
 }
--- Scripts/Player.js
+++ Scripts/Player.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById("gameCanvas");
+var flipPlayer = false;
 
 var Player = function() {
 	this.image = document.createElement("img");
@@ -7,8 +8,8 @@ var Player = function() {
 	this.xPos = canvas.width/2;
 	this.yPos = canvas.height/2;
 	
-	this.width = 159;
-	this.height = 163;
+	this.width = 119;
+	this.height = 103;
 	
 	this.velocityX = 0;
 	this.velocityY = 0;
@@ -21,17 +22,48 @@ var Player = function() {
 };
 
 Player.prototype.update = function(deltaTime){
-	if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true){
-		this.rotation -= deltaTime;
-	}else{
-		this.rotation += deltaTime;
+	if(this.xPos > canvas.width - (this.width / 2)){
+		this.xPos = canvas.width - (this.width / 2);
+	}
+	if(this.xPos < 0 + (this.width / 2)){
+		this.xPos = 0 + (this.width / 2);
+	}
+	if(this.yPos > canvas.height - (this.height / 2)){
+		this.yPos = canvas.height - (this.height / 2);
+	}
+	if(this.yPos < 0 + (this.width / 2) - 7){
+		this.yPos = 0 + (this.width / 2) - 7;
+	}
+
+	if(keyboard.isKeyDown(keyboard.KEY_W) == true){
+		this.yPos -= 5;
+	}
+	if(keyboard.isKeyDown(keyboard.KEY_S) == true){
+		this.yPos += 5;
+	}
+	if(keyboard.isKeyDown(keyboard.KEY_A) == true){
+		this.xPos -= 5;
+		flipPlayer = true;
+	}
+	if(keyboard.isKeyDown(keyboard.KEY_D) == true){
+		this.xPos += 5;
+		flipPlayer = false;
 	}
 }
 
 Player.prototype.draw = function(){
-	context.save();
-		context.translate(this.a.xPos, this.a.yPos);
-		context.rotate(this.rotation);
-		context.drawImage(this.image, -this.width/2, -this.height/2);
-	context.restore();
+	if(flipPlayer == true){
+		context.save();
+			context.translate(this.width, 0);
+			context.translate(this.xPos, this.yPos);
+			context.scale(-1, 1);
+			context.drawImage(this.image, +this.width/2, -this.height/2);
+		context.restore();
+	}else{
+		context.save();
+			context.translate(this.xPos, this.yPos);
+			context.rotate(this.rotation);
+			context.drawImage(this.image, -this.width/2, -this.height/2);
+		context.restore();
+	}
 }
